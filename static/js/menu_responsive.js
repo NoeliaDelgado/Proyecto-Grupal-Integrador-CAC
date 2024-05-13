16987225e18abed33033bc7e97700474ea7d4d85
Silvina Delgado <silvinadelgadotridon@gmail.com>
2024-05-13 14:18:31 -0300

    hago archivo menu_responsive.js y lo pruebo para index.html, favicon, iframe, cambio contacto.html por turnos.html

--- static/js/menu_responsive.js
+++ static/js/menu_responsive.js
@@ -7,8 +7,8 @@ document.querySelector(".btn-menuResponsive").addEventListener("click", toggleMe
 
 function toggleMenu() {
     nav.classList.toggle("show");
-    for (let i = 0; i < itemsNav.length; i++) {
-        itemsNav[i].addEventListener("click", function () {
+    for (let i of itemsNav) {
+        i.addEventListener("click", function () {
             nav.classList.remove("show");
         });
     }
