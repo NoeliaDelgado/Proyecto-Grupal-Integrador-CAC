56fda1c139dad89244e107b263b7a5d3bc8878b9
Noelia <delgado.marianoelia@gmail.com>
2024-05-23 22:26:33 -0300

    todo validado

--- static/js/ver_mas.js
+++ static/js/ver_mas.js
@@ -6,7 +6,7 @@ let verMas = document.querySelectorAll('.ver-mas');
 verMas.forEach((boton) => {
     boton.addEventListener('click', () => {
         let contenido = boton.parentElement.nextElementSibling;
-        let arrow = boton.querySelector('#arrow');
+        let arrow = boton.querySelector('.arrow');
         
         contenido.classList.toggle('oculto');
         arrow.classList.toggle('rotate');
